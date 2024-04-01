c10f8290a414d0ef254e9e884a204458d68db22f
viper-108 <85250221+viper-108@users.noreply.github.com>
2024-04-01 14:26:41 +0530

    Add files via upload

---
+++ deploy_google_cloud_run.sh
@@ -0,0 +1,1 @@
+gcloud run deploy --image gcr.io/calm-cairn-292101/allindor_app --platform managed
---
+++ flask_key.sh
@@ -0,0 +1,8 @@
+export FLASK_SECRET_KEY='f99719a8da8e3bffeed7cc873786c343'
+
+# Here is the code to get the flask secret code
+
+# import os
+# import secrets
+# secret_key = secrets.token_hex(16)
+# print(secret_key)
